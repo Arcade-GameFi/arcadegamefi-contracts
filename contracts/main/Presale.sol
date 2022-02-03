91381d400775ac4e0436d5b448b47e5634701024
masudbarki <61346065+masudrehman@users.noreply.github.com>
2022-02-03 13:48:55 +0500

    ERC20 is not inherited..
    ERC20 is not inherited. Inherit it to have access to ERC20.decimals().

--- contracts/main/Presale.sol
+++ contracts/main/Presale.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract Presale is Ownable {
+contract Presale is Ownable, ERC20 {
     using SafeERC20 for IERC20;
 
     struct SaleInfo {
